ff6ab508d64538dbfd908a8cfc7ed606034ad185
oszso <43454692+oszso@users.noreply.github.com>
2018-09-20 16:33:46 -0500

    Update Tarea.R

--- Tarea.R
+++ Tarea.R
@@ -1,12 +1,14 @@
 library(vcd)
-###Asociación entre el tratamiento y niveles de mejora
+###AsociaciÃ³n entre el tratamiento y niveles de mejora
 
 ###Tabla de contingencia
 prop.table(table(Arthritis$Treatmen,Arthritis$Improved))
 TE<-table(Arthritis$Treatment,Arthritis$Improved)
+barplot(TE, main="Tratamiento",col=c("darkblue","darkred"),legend=rownames(TE),beside=TRUE)
+
 assoc(TE,color=TRUE,shade=T,main="mosaico")
 
-barplot(TE, main="Tratamiento",col=c("darkblue","darkred"),legend=rownames(TE),beside=TRUE)
+##Pvalue=0.0014626
 
 mosaicplot(TE, color=TRUE,split=TRUE,shade=TRUE,main="mosaico")
 
@@ -16,12 +18,13 @@ chisq.test(TE)
 qchisq(0.95,2)
 chisq.test(TE)$expected
 
-###Asociación entre el sexo y niveles de mejora
+###AsociaciÃ³n entre el sexo y niveles de mejora
 TS<-table(Arthritis$Sex,Arthritis$Improved)
-assoc(TE,color=TRUE,shade=T,main="mosaico")
-
 barplot(TS, main="Tratamiento",col=c("darkblue","darkred"),legend=rownames(TS),beside=TRUE)
 
+assoc(TS,color=TRUE,shade=T,main="mosaico")
+
+##Pvalue=.088891
 mosaicplot(TS,color=TRUE,shade=TRUE,main="mosaico")
 
 assoc(TS,color=TRUE,shade=T,main="mosaico",col=c("darkblue","darkred"))
